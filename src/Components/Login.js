f34a7a248befe9b0d21031afebe25eea377e9ffd
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2023-02-04 22:08:25 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -20,7 +20,7 @@ const Login = ({ setLoginUser }) => {
     }
 
     const login = () => {
-        axios.post("http://localhost:9002/login", user)
+        axios.post("http://nasa-backend-drab.vercel.app/login", user)
             .then(res => {
                 navigate("/nasaphoto")
             })
